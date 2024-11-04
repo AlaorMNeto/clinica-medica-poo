4edba193d0102cd7999ba3a4c0a00e84affc8443
AlaorMNeto <alaor.neto@aluno.imepac.edu.br>
2024-11-03 21:07:13 -0300

    Criacao da classe Convenio

--- src/main/java/br/edu/imepac/administrativo/entidades/Convenio.java
+++ src/main/java/br/edu/imepac/administrativo/entidades/Convenio.java
@@ -1,5 +1,16 @@
 package br.edu.imepac.administrativo.entidades;
 
 public class Convenio {
+  private String nome_empresa;
+    private String CNPJ;
+    private String telefone;
 
+    public void setNome_empresa(String nome_empresa) { this.nome_empresa = nome_empresa; }
+    public String getNome_empresa() { return nome_empresa; }
+
+    public void setCNPJ(String CNPJ) { this.CNPJ = CNPJ; }
+    public String getCNPJ() { return CNPJ; }
+
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+    public String getTelefone() { return telefone; }
 }
